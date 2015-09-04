178df9ceed16d2d52f5d2e66014b3c413a95f94d
Chung Jason <shiningjason1989@gmail.com>
2015-09-04 12:22:58 +0800

    Step 1. First Component

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
+import Header from './Header';
 
 const App = React.createClass({
   render() {
-    var codeBlockStyle = { "fontFamily": "monospace",
-                           "backgroundColor": "#D0D0D0" };
     return (
-    <div>
-      <header>
-        <h1>react-flux-todos</h1>
-        <h3>A React/Flux app generate by RF, powered with Babel</h3>
-      </header>
-      <article className="context">
-        <p>
-          Greeting form <a href="https://github.com/taiansu/generator-rf">RF generator</a>.
-        </p>
-
-        <p>
-          Remember you are powered with <a href="https://gaearon.github.io/react-hot-loader/">react-hot-loader</a> now. Edit this file in <span style={ codeBlockStyle }>src/scripts/components/App.js</span> and save it, it will auto reload the page for you.
-        </p>
-      </article>
-    </div>
+      <div>
+        <Header />
+        <div>
+          Here is todos block!
+        </div>
+      </div>
     );
   }
 });
---
+++ src/scripts/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = React.createClass({
+  render() {
+    return (
+      <header>
+        <h1>todos</h1>
+        <p>Hay, Jason, this is your todos!</p>
+      </header>
+    );
+  }
+});
+
+module.exports = Header;
