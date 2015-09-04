1dd0bb32a5da50dd6830d40f8d1a9da7af502211
Chung Jason <shiningjason1989@gmail.com>
2015-09-04 13:47:19 +0800

    Step 2. Component Composition

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -5,7 +5,7 @@ const App = React.createClass({
   render() {
     return (
       <div>
-        <Header />
+        <Header username="Jason" todoNumber={0} />
         <div>
           Here is todos block!
         </div>
--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const Header = React.createClass({
+
+  propTypes: {
+    username: React.PropTypes.string.isRequired,
+    todoNumber: React.PropTypes.number.isRequired
+  },
+
   render() {
+    const { username, todoNumber } = this.props;
+
     return (
       <header>
         <h1>todos</h1>
-        <p>Hay, Jason, this is your todos!</p>
+        <p>哈囉，{username}：您今天有 {todoNumber} 條代辦事項！</p>
       </header>
     );
   }
